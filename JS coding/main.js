63cbd57f70bca04b9b57aa1540b0cedebb17cd34
Stabbo94 <albertostaboli@gmail.com>
2025-08-24 10:35:22 +0200

    Allego esercizio

---
+++ JS coding/main.js
@@ -0,0 +1,26 @@
+let num1 = 5;
+
+let num2 = 12;
+
+console.log('Prima variabile è ' + num1);
+
+console.log('Seconda variabile è ' + num2);
+
+let stringa = "Fabio"
+
+console.log('La stringa è ' + stringa)
+
+const PIGRECO = 3.14159265359
+
+console.log(PIGRECO)
+
+PIGRECO=5.2
+
+console.log(PIGRECO)
+
+
+
+
+
+
+
